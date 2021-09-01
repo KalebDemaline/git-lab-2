0a677f307a5235111bb702fb02811b9108a864c1
KalebDemaline <kd148920@ohio.edu>
2021-09-01 17:13:44 -0400

    Intial commit

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Kaleb Demaline
+ *        Date: 9/01/2020
+ * Description: lab 2 code
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
